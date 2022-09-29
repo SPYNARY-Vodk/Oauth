459f7c186bca31e5f34204fc58cc5c0973e996e2
SPYNARY-Vodk <111708405+SPYNARY-Vodk@users.noreply.github.com>
2022-09-29 16:53:58 +0200

    Update oauth.js

--- oauth.js
+++ oauth.js
@@ -2,6 +2,6 @@ const Client = require('disco-oauth');
 const client = new Client("1011611413657169960", "pS0z3-HVoyCitJuUKsq_3iuzFkmyGFFC");
 
 client.setScopes('identify', 'guilds');
-client.setRedirect('https://before-unite.herokuapp.com/login');
+client.setRedirect('https://before-united.herokuapp.com/login');
 
 module.exports = client;
